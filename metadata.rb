4b0d013a80b136a73b705426f729d6ee22013252
ken.pierson <kpierson@screenengineasi.com>
2019-10-21 16:41:29 -0700

    add metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'passenger-nginx'
+maintainer       'Ken Pierson'
+maintainer_email 'kspierson@gmail.com'
+license          'MIT'
+description      'Installs/Configures Passenger with Nginx'
+long_description 'Installs/Configures RVM, Ruby, Phusion Passenger (open source and Enterprise editions) with Nginx'
+version          '0.9.21'
+
+supports 'awslinux'
