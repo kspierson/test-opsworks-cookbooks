f3ab83be2df008c71351d2afe3db2e99e187b5a6
ken.pierson <kpierson@screenengineasi.com>
2019-10-23 13:56:12 -0700

    change gpg keys

--- passenger_nginx/recipes/default.rb
+++ passenger_nginx/recipes/default.rb
@@ -34,8 +34,9 @@ end
 end
 
 execute "Installing GPG keys so that RVM won't barf on installation" do
-  command "curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -"
-  command "curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg --import -"
+  #command "curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -"
+  #command "curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg --import -"
+  command "sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
 
   user "root"
   not_if { File.exists? "/usr/local/rvm/bin/rvm" }
