58a14d47355d8f5c14d92b06457a54062e81063d
ken.pierson <kpierson@screenengineasi.com>
2019-10-23 15:51:09 -0700

    use gpg2 for keys

--- passenger_nginx/recipes/default.rb
+++ passenger_nginx/recipes/default.rb
@@ -37,7 +37,7 @@ end
 execute "Installing GPG keys so that RVM won't barf on installation" do
   #command "curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -"
   #command "curl -sSL https://rvm.io/pkuczynski.asc | sudo gpg --import -"
-  command "sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+  command "sudo gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
 
   user "root"
   not_if { File.exists? "/usr/local/rvm/bin/rvm" }
