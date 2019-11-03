a142f64839d332639b7cf0b764951bb78cf4777f
ken.pierson <kpierson@screenengineasi.com>
2019-11-03 15:44:20 -0800

    add EOF to bash

--- passenger_nginx/recipes/deploy.rb
+++ passenger_nginx/recipes/deploy.rb
@@ -22,9 +22,10 @@ execute "Installing NVM" do
 end
 
 bash "Configuring NVM" do
-  code 'export NVM_DIR="$HOME/.nvm"
+  code <<-EOF
+    export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"'
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
   EOF
   user "root"
   not_if { File.exists? "/opt/nginx/sbin/nginx" }
