dd077a018ff0aa63f13536d61f201fb6b08eba73
ken.pierson <kpierson@screenengineasi.com>
2019-11-03 12:53:45 -0800

    include deploy attribute

--- passenger_nginx/recipes/deploy.rb
+++ passenger_nginx/recipes/deploy.rb
@@ -1,3 +1,5 @@
+include_attribute "deploy"
+
 # Install Node
 execute "Installing NodeJS" do
   command "curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -"
@@ -19,28 +21,28 @@ end
 # Download and deploy
 file '/root/.ssh/id_rsa' do
   mode '0400'
-  content "#{deploy['preview_free_movies']['scm']['ssh_key']}"
+  content "#{node['deploy']['preview_free_movies']['scm']['ssh_key']}"
 end
 
 execute "Downloading and Deploying..." do
-  command "git clone -b #{deploy['preview_free_movies']['scm']['revision']} --single-branch #{deploy['preview_free_movies']['scm']['repository']} ."
+  command "git clone -b #{node['deploy']['preview_free_movies']['scm']['revision']} --single-branch #{node['deploy']['preview_free_movies']['scm']['repository']} ."
   command "sudo yum install -y nodejs"
 
   user "root"
-  cwd "#{deploy['preview_free_movies']['deploy_to']}"
+  cwd "#{node['deploy']['preview_free_movies']['deploy_to']}"
   not_if { File.exists? "/usr/local/bin/node" }
 end
 
 # git '/var/www' do
-#   repository "#{deploy['preview_free_movies']['scm']['repository']}"
-#   revision "#{deploy['preview_free_movies']['scm']['revision']}"
+#   repository "#{node['deploy']['preview_free_movies']['scm']['repository']}"
+#   revision "#{node['deploy']['preview_free_movies']['scm']['revision']}"
 # end
 
 # install NPM packages
 execute 'Installing NPM Packages' do
   command 'npm prune'
   command 'npm install'
-  cwd "#{deploy['preview_free_movies']['deploy_to']}"
+  cwd "#{node['deploy']['preview_free_movies']['deploy_to']}"
 end
 
 # start the server
