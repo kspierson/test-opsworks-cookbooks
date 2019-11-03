b866bd6772818f4da287f5a7a9c954a3042ab75d
ken.pierson <kpierson@screenengineasi.com>
2019-11-03 13:36:37 -0800

    get deploy info from data bag json

--- passenger_nginx/recipes/deploy.rb
+++ passenger_nginx/recipes/deploy.rb
@@ -1,3 +1,5 @@
+app = search("aws_opsworks_app").first
+
 # Install Node
 execute "Installing NodeJS" do
   command "curl -sL https://rpm.nodesource.com/setup_12.x | sudo -E bash -"
@@ -7,40 +9,40 @@ execute "Installing NodeJS" do
   not_if { File.exists? "/usr/local/bin/node" }
 end
 
-if node
-  Chef::Log.info("Reading node...")
-  node.each do |app, deploy|
-      Chef::Log.info("node -#{ app }-")
-  end
-elsif
-  Chef::Log.info("NO NODE")
-end
+# if node
+#   Chef::Log.info("Reading node...")
+#   node.each do |app, deploy|
+#       Chef::Log.info("node -#{ app }-")
+#   end
+# elsif
+#   Chef::Log.info("NO NODE")
+# end
 
 # Download and deploy
 file '/root/.ssh/id_rsa' do
   mode '0400'
-  content "#{node['deploy']['preview_free_movies']['scm']['ssh_key']}"
+  content "#{app['app_source']['ssh_key']}"
 end
 
 execute "Downloading and Deploying..." do
-  command "git clone -b #{node['deploy']['preview_free_movies']['scm']['revision']} --single-branch #{node['deploy']['preview_free_movies']['scm']['repository']} ."
+  command "git clone -b #{app['app_source']['revision']} --single-branch #{app['app_source']['url']} ."
   command "sudo yum install -y nodejs"
 
   user "root"
-  cwd "#{node['deploy']['preview_free_movies']['deploy_to']}"
+  cwd "#{app['app_source']['deploy_to']}"
   not_if { File.exists? "/usr/local/bin/node" }
 end
 
 # git '/var/www' do
-#   repository "#{node['deploy']['preview_free_movies']['scm']['repository']}"
-#   revision "#{node['deploy']['preview_free_movies']['scm']['revision']}"
+#   repository "#{app['app_source']['repository']}"
+#   revision "#{app['app_source']['revision']}"
 # end
 
 # install NPM packages
 execute 'Installing NPM Packages' do
   command 'npm prune'
   command 'npm install'
-  cwd "#{node['deploy']['preview_free_movies']['deploy_to']}"
+  cwd "#{app['attributes']['document_root']}"
 end
 
 # start the server
