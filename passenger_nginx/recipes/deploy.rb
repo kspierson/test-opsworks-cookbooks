2468170bb677032a13b47ba22ebd39ac0f941f40
ken.pierson <kpierson@screenengineasi.com>
2019-11-03 14:18:23 -0800

    log output

--- passenger_nginx/recipes/deploy.rb
+++ passenger_nginx/recipes/deploy.rb
@@ -15,8 +15,9 @@ execute "Installing NVM and Node" do
   command "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash"
   command ". ~/.nvm/nvm.sh"
   command "nvm install node 10.15.2"
-  output = shell_out!('node -v').stdout
-  Chef::Log.info(output)
+  Chef::Log.info(shell_out!('nvm -v').stdout)
+  Chef::Log.info(shell_out!('nvm list').stdout)
+  Chef::Log.info(shell_out!('node -v').stdout)
 
   user "root"
   #not_if { File.exists? "/usr/local/bin/node" }
