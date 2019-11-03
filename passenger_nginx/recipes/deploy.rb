adbcd5b02ee50252dd21e4ba40065c01f10e07f4
ken.pierson <kpierson@screenengineasi.com>
2019-11-03 15:29:46 -0800

    change to bash

--- passenger_nginx/recipes/deploy.rb
+++ passenger_nginx/recipes/deploy.rb
@@ -22,12 +22,9 @@ execute "Installing NVM" do
 end
 
 bash "Configuring NVM" do
-  code <<-EOF
-    cat <<EOF >> .bashrc
-    export NVM_DIR="$HOME/.nvm"
-    && \. "$NVM_DIR/nvm.sh"
-    && \. "$NVM_DIR/bash_completion"
-    EOF
+  code 'export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"'
   EOF
   user "root"
   not_if { File.exists? "/opt/nginx/sbin/nginx" }
