5d47fa605e63298d08610990a768b000898b9b1e
ken.pierson <kpierson@screenengineasi.com>
2019-11-04 00:47:23 -0800

    app_source to attributes

--- passenger_nginx/recipes/deploy.rb
+++ passenger_nginx/recipes/deploy.rb
@@ -130,10 +130,10 @@ end
 #   IdentitiesOnly yes # see NOTES below"
 # end
 
-directory "#{app['app_source']['document_root']}" do
+directory "#{app['attributes']['document_root']}" do
   mode 0755
   action :create
-  not_if { File.directory? "#{app['app_source']['document_root']}" }
+  not_if { File.directory? "#{app['attributes']['document_root']}" }
 end
 
 # execute "Creating directory..." do
@@ -149,7 +149,7 @@ execute "Downloading and Deploying..." do
   #command "git clone -b #{app['app_source']['revision']} --single-branch #{app['app_source']['url']} ."
 
   user "root"
-  cwd "#{app['app_source']['document_root']}"
+  cwd "#{app['attributes']['document_root']}"
   #not_if { File.exists? "/usr/local/bin/node" }
 end
 
