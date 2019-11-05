8de5e4f38c0c9ef3a6b6f3eb47df44f3396f51fb
ken.pierson <kpierson@screenengineasi.com>
2019-11-04 16:48:10 -0800

    update log message

--- passenger_nginx/recipes/deploy.rb
+++ passenger_nginx/recipes/deploy.rb
@@ -31,11 +31,11 @@ end
 
 execute "ls -la" do
   # Chef::Log.info(shell_out!("ls -la").stdout)
-  Chef::Log.info(shell_out!("ls -la /usr/bin").stdout)
-  Chef::Log.info(shell_out!("ls -la /usr/local/bin").stdout)
-  Chef::Log.info(shell_out!("ls -la ~/.nvm").stdout)
-  Chef::Log.info(shell_out!("ls -la ~").stdout)
-  #Chef::Log.info(shell_out!("sudo which node").stdout)
+  # Chef::Log.info(shell_out!("ls -la /usr/bin").stdout)
+  # Chef::Log.info(shell_out!("ls -la /usr/local/bin").stdout)
+  Chef::Log.info(shell_out!("ls -la ~/.nvm/versions/node/").stdout)
+  # Chef::Log.info(shell_out!("ls -la ~").stdout)
+  Chef::Log.info(shell_out!("nvm which node").stdout)
 
   user "root"
 end
