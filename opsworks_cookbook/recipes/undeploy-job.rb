5a9a2771d83802b2c9c31e2c9b38ee8bb32d0ebf
ken.pierson <kpierson@screenengineasi.com>
2019-11-20 17:30:07 -0800

    cleanup

--- opsworks_cookbook/recipes/undeploy-job.rb
+++ opsworks_cookbook/recipes/undeploy-job.rb
@@ -1,35 +1,25 @@
 # Remove Cron Jobs
 cron 'Screening Snapshot' do
-  # minute '*/7'
-  # command 'NODE_ENV=production flock -n /tmp/takeScreeningSnapshot.lock /usr/local/bin/node /var/www/server/build/server/src/scripts/takeScreeningSnapshot.js >> /home/ec2-user/logs/snapshotCron.log'
   user 'ec2-user'
   action :delete
 end
 
 cron 'Screening Reminder' do
-  # minute '*/30'
-  # command 'NODE_ENV=production flock -n /tmp/sendScreeningReminder.lock /usr/local/bin/node /var/www/server/build/server/src/scripts/sendScreeningReminder.js >> /home/ec2-user/logs/reminderCron.log'
   user 'ec2-user'
   action :delete
 end
 
 cron 'Create DB Snapshot' do
-  # minute '*'
-  # command 'NODE_ENV=production flock -n /tmp/takeDbSnapshot.lock /usr/local/bin/node /var/www/server/build/server/src/scripts/takeDbSnapshot.js >> /home/ec2-user/logs/dbSnapshotCron.log'
   user 'ec2-user'
   action :delete
 end
 
 cron 'Update Screening Invitation' do
-  # minute '*/30'
-  # command 'NODE_ENV=production flock -n /tmp/updateScreeningInvitation.lock /usr/local/bin/node /var/www/server/build/server/src/scripts/updateScreeningInvitation.js >> /home/ec2-user/logs/screeningInvitationCron.log'
   user 'ec2-user'
   action :delete
 end
 
 cron 'Generate RDS Token' do
-  # minute '*/14'
-  # command 'NODE_ENV=production flock -n /tmp/generateRDSToken.lock /usr/local/bin/node /var/www/server/build/server/src/scripts/generateRDSToken.js >> /home/ec2-user/logs/generateRDSToken.log'
   user 'ec2-user'
   action :delete
 end
@@ -39,11 +29,4 @@ execute "Removing Deployment" do
   command "rm -rf /var/www"
 
   #user "ec2-user"
-end
-
-ruby_block 'Crontab Output' do
-  block do
-    Chef::Log.info(shell_out!("crontab -u ec2-user -l").stdout)
-  end
-  action :run
 end
