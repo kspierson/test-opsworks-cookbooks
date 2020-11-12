3cab9d65cec6f8431f45224ab9e196b3bc651c42
ken.pierson <kpierson@screenengineasi.com>
2020-11-12 14:40:57 -0800

    undo key change

--- opsworks_cookbook/recipes/setup.rb
+++ opsworks_cookbook/recipes/setup.rb
@@ -11,7 +11,7 @@ end
 end
 
 execute "Installing GPG keys" do
-  command "sudo gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
+  command "sudo gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
 
   user "root"
   not_if { File.exists? "/usr/local/rvm/bin/rvm" }
