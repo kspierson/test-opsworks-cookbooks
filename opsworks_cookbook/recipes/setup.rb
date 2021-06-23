f18592068cde0d606d38d6b7a22633b28cef49db
ken.pierson <kpierson@screenengineasi.com>
2021-06-23 09:54:48 -0700

    add retry to gpg key install

--- opsworks_cookbook/recipes/setup.rb
+++ opsworks_cookbook/recipes/setup.rb
@@ -51,6 +51,7 @@ execute "Installing GPG keys" do
   command "gpg2 --homedir /root/.gnupg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
 
   user "root"
+  retries 5
   not_if { File.exists? "/usr/local/rvm/bin/rvm" }
 end
 
